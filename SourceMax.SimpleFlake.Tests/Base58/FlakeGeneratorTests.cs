23530ae3ed22a8443d087ce107869cb915930a0f
cwiederspan <chris@wiederspan.com>
2015-03-19 15:34:44 -0600

    Added additional tests

---
+++ SourceMax.SimpleFlake.Tests/Base58/FlakeGeneratorTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using SUT = SourceMax.SimpleFlake.Base58;
+
+namespace SourceMax.SimpleFlake.Tests.Base58 {
+
+    [TestClass]
+    public class FlakeGeneratorTests {
+
+        [TestMethod]
+        public void TestMethod1() {
+
+            var generator = new SUT.FlakeGenerator();
+            var id = generator.CreateFlake().ToString();
+        }
+    }
+}
---
+++ SourceMax.SimpleFlake.Tests/FlakeGeneratorTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SourceMax.SimpleFlake.Tests {
+
+    [TestClass]
+    public class FlakeGeneratorTests {
+
+        [TestMethod]
+        public void TestMethod1() {
+
+            var generator = new FlakeGenerator<Flake>();
+            var id = generator.CreateFlake().ToString();
+        }
+    }
+}
